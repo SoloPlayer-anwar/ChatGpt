c60f0f3f8fc8d3cd2765595dbb39e3f252d63af3
SoloPlayer-anwar <arabainahhj@gmail.com>
2023-04-27 00:02:07 +0700

    first commit

--- app/src/main/java/com/twincom/chatgpt/MainActivity.kt
+++ app/src/main/java/com/twincom/chatgpt/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.twincom.chatgpt
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.twincom.chatgpt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
+    private var loadingDialog: Dialog? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    }
 
+    }
 }
--- app/src/main/java/com/twincom/chatgpt/network/EndPoint.kt
+++ app/src/main/java/com/twincom/chatgpt/network/EndPoint.kt
@@ -1,21 +1,19 @@
 package com.twincom.chatgpt.network
 
-
-
-
 import com.twincom.chatgpt.requestbody.RequestBody
-import com.twincom.chatgpt.response.ChatGptResponse
+import com.twincom.chatgpt.response.OpenAiResponse
 import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
+
 interface EndPoint {
     @POST("completions")
-    fun submitChatGpt(
-        @Header("Content-Type") contentType: String,
-        @Header("Authorization") authHeader: String,
+    fun submitOpenAi(
+        @Header("Content-Type") contentType:String,
+        @Header("Authorization") authHeader:String,
         @Body requestBody: RequestBody
-    ) : Observable<Response<ChatGptResponse>>
+    ): Observable<Response<OpenAiResponse>>
 }
